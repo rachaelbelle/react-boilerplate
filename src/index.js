c4d59c99902e672d943c7bac4a080d27b482482f
YvelStLaurent <rachaelbelle@gmail.com>
2019-07-11 19:50:24 -0400

    added sass

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./styles.scss"
 const Index = () => {
-  return <div>You are amazing Yvel! Welcome to React!!</div>;
+  return <div id="home">
+    <p className="welcome">You are amazing Yvel! Welcome to React!!</p>
+      </div>;
 };
 ReactDOM.render(<Index />, document.getElementById('root'));
