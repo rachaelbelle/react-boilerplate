2fb4888b78e9011d3122017d97b8b11608211940
YvelStLaurent <rachaelbelle@gmail.com>
2019-07-11 19:32:51 -0400

    react working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./styles.scss"
 const Index = () => {
-  return <div>Welcome to React!</div>;
+  return <div>You are amazing Yvel! Welcome to React!!</div>;
 };
 ReactDOM.render(<Index />, document.getElementById('root'));
