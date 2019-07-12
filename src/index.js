7b6962f09377a1c4136e2244b66d07630cf565a0
YvelStLaurent <rachaelbelle@gmail.com>
2019-07-11 20:36:18 -0400

    building out app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "./styles.scss"
-const Index = () => {
-  return <div id="home">
-    <p className="welcome">You are amazing Yvel! Welcome to React!!</p>
-      </div>;
-};
-ReactDOM.render(<Index />, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './styles.scss'
+
+ReactDOM.render(<App />, document.getElementById('root'))
